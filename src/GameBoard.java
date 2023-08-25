5184f70d40c727861c537b3f79f3ffa9ec129fcf
Michael King <michael.king1299@gmail.com>
2023-08-25 13:24:27 +0100

    just progress

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class GameBoard {
@@ -12,28 +13,40 @@ public class GameBoard {
     private int timesInvoked;
 
     // multi-dimensional array to simulate game board.
-    char [][] gameBoard = new char[BOARD_SIZE][BOARD_SIZE];
+    private char [][] gameBoard = new char[BOARD_SIZE][BOARD_SIZE];
 
-    // Initialise array list for the coordinates.
-    ArrayList<Coordinate> coordinates = new ArrayList<>();
 
     // Initialisation of the battleships and their properties.
-    ArrayList<Battleship> ships = new ArrayList<>();
-    Battleship aircraftCarrier = new Battleship("Aircraft Carrier", 5);
-    Battleship battleship = new Battleship("Battleship", 4);
-    Battleship submarine = new Battleship("Submarine", 3);
-    Battleship cruiser = new Battleship("Cruiser", 3);
-    Battleship destroyer = new Battleship("Destroyer", 2);
+    private ArrayList<Battleship> ships = new ArrayList<>();
+    private Battleship aircraftCarrier = new Battleship("Aircraft Carrier", 5);
+    private Battleship battleship = new Battleship("Battleship", 4);
+    private Battleship submarine = new Battleship("Submarine", 3);
+    private Battleship cruiser = new Battleship("Cruiser", 3);
+    private Battleship destroyer = new Battleship("Destroyer", 2);
 
     // Initialise scanner for user input.
-    Scanner scanner = new Scanner(System.in);
+    private Scanner scanner = new Scanner(System.in);
 
     // Methods
 
     /**
-     *
+     * Method to encapsulate important game methods.
      */
     public void beginGame() {
+
+        // Print out initial board.
+        printBoard(gameBoard);
+
+        // present the user with their battleships.
+        battleshipsSetup();
+    }
+
+    /**
+     * Add ships to the arraylist to be looped over.
+     * Each ship taking input from user on where they should be placed on the game board.
+     */
+    private void battleshipsSetup() {
+
         // Add the ships to the Arraylist.
         ships.add(aircraftCarrier);
         ships.add(battleship);
@@ -41,8 +54,6 @@ public class GameBoard {
         ships.add(cruiser);
         ships.add(destroyer);
 
-        // Print out initial board.
-        printBoard(gameBoard);
 
         // Loop over ship objects by getting their names and sizes from Arraylist.
         for (Battleship ship : ships) {
@@ -55,17 +66,16 @@ public class GameBoard {
             String second = scanner.next();
             Coordinate secondCoordinate = readInput(second);
 
-            placeShips(firstCoordinate, secondCoordinate);
+            placeShips(firstCoordinate, secondCoordinate, ship.getSize(), ship.getName());
         }
-        scanner.close(); // Rejig this, just separate co ords here? into ints, pass into placeShips func.
+        scanner.close();
     }
 
     /**
-     *
-     * @param coordinate
-     * @return coordinate obj
+     * @param coordinate take coordinate input as a string from user.
+     * @return coordinate obj which stores X , Y as ints.
      */
-    public Coordinate readInput(String coordinate) {
+    private Coordinate readInput(String coordinate) {
 
         char inputRow = coordinate.charAt(0); // get char for row input.
         int inputCol = Integer.parseInt(coordinate.substring(1)); // get int for col input.
@@ -77,11 +87,11 @@ public class GameBoard {
     }
 
     /**
-     *
-     * @param c1
-     * @param c2
+     * @param c1 take first coordinate where ship will start.
+     * @param c2 take second coordinate where ship will end.
+     * @param shipSize make sure given coordinates match up to the ships size.
     */
-    public void placeShips(Coordinate c1, Coordinate c2) {
+    private void placeShips(Coordinate c1, Coordinate c2, int shipSize, String shipName) {
 
         // first coordinate.
         int firstRow = c1.getX();
@@ -91,12 +101,23 @@ public class GameBoard {
         int secondRow = c2.getX();
         int secondCol = c2.getY();
 
-        // Take coordinate points and fill the gaps between them.
-        if (coordinatesValid(firstRow, firstCol, BOARD_SIZE) && coordinatesValid(secondRow, secondCol, BOARD_SIZE)) {
-            for (int row = firstRow; row <= secondRow; row++) {
-                for (int col = firstCol; col <= secondCol; col++) gameBoard[row][col] = SHIP_CHAR;
+        // Get lengths of the coordinates placed.
+        int shipsLengthRow = secondRow - firstRow;
+        int shipsLengthCol = secondCol - firstCol;
+
+        try {
+            if (shipsLengthRow + 1 > shipSize || shipsLengthCol + 1 > shipSize) {
+                // Needs to take user back to specific part of the loop.
+                System.out.println("Error! Wrong length of the " + shipName + "! Try again:");
+                beginGame();
             }
-        } else {
+            // Take coordinate points and fill the gaps between them.
+            if (coordinatesValid(firstRow, firstCol) && coordinatesValid(secondRow, secondCol)) {
+                for (int row = firstRow; row <= secondRow; row++) {
+                    for (int col = firstCol; col <= secondCol; col++) gameBoard[row][col] = SHIP_CHAR;
+                }
+            }
+        } catch (InputMismatchException e) {
             System.out.println("Invalid coordinates. Please enter valid coordinates.");
         }
 
@@ -105,10 +126,9 @@ public class GameBoard {
     }
 
     /**
-     *
-     * @param gameBoard
+     * @param gameBoard take global game-board array so it can be updated.
      */
-    public void printBoard(char[][] gameBoard) {
+    private void printBoard(char[][] gameBoard) {
 
         // Introduce the boards rows and columns as local variables.
         char rowChar = 'A';
@@ -144,15 +164,21 @@ public class GameBoard {
     }
 
     /**
-     *
-     * @param row
-     * @param col
-     * @param boardSize
-     * @return
+     * @param row make sure provided row is within the game limits.
+     * @param col make sure provided column is within the game limits.
+     * @return true if provided arguments are within the limits. Else, fail.
      */
-    public boolean coordinatesValid(int row, int col, int boardSize) {
-        return row >= 0 && row < boardSize && col >= 0 && col < boardSize;
+    private boolean coordinatesValid(int row, int col) {
+        return row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE;
     }
 }
+
+/**
+ * Introduce desired error handling.
+ *      ** Co ordinates not in the same row // column.
+ *      ** Ships too close // ontop of each other.
+ *      ** finish over sizing.
+ *      ** Just wrong input in general.
+ */
 
 
