ca486494c49739d4d2eae5986c38c963935e4eea
Michael King <michael.king1299@gmail.com>
2023-08-28 15:45:07 +0100

    Cleaned up battleship list.

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,13 +1,10 @@
 public class Coordinate {
     private int x; // row
     private int y; // col
-
     public Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
     }
-
     public int getX() { return x; }
-
     public int getY() { return y; }
 }
